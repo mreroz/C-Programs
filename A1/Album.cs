49f6c81018fbbbff060517682fcec149bbc1ac5c
Eros Costa <95498828+mreroz@users.noreply.github.com>
2026-02-07 11:09:37 +0000

    Improve order

---
+++ A1/Album.cs
@@ -0,0 +1,9 @@
+public class Album
+{
+    // Album name, artist name, and number of tracks
+    private string albumName; // Name of the album
+    private string artistName; // Name of the artist
+    private int numOfTracks; // Number of tracks in the album
+    
+    //more code
+}
